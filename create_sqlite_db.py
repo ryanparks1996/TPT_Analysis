2f4186caebdbdcd4bad44a102c0058b44603f570
ryanparks1996 <ryan.j.parks1996@gmail.com>
2025-10-20 17:19:35 -0400

    Add files via upload

---
+++ create_sqlite_db.py
@@ -0,0 +1,14 @@
+import sqlite3
+import pandas as pd
+
+# Load data file
+df = pd.read_csv("C:/Users/ryans/Documents/TPT Project/Copy of Average TPT.csv")
+
+#Create SQLite dataabase
+connection = sqlite3.connect('C:/Users/ryans/Documents/TPT Project/TPT.db')
+
+#load data file to database
+df.to_sql('weekly_tpt', connection, if_exists='replace')
+
+#close connection
+connection.close()
